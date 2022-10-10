e7d88f059117af57c7d28be7e8c82a2ac1ca1f02
Dolbocat <Delorkin@yandex.ru>
2022-10-10 10:15:57 +0300

    Домашняя работа 3

---
+++ Complecses/Complex.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+namespace Complecses
+{
+
+
+    struct Complex
+    {
+        public double im;
+        public double re;
+        
+        public Complex Plus(Complex x)
+        {
+            Complex y;
+            y.im = im + x.im;
+            y.re = re + x.re;
+            return y;
+        }
+        public Complex Subtract(Complex x)
+        {
+            Complex y;
+            y.im = im - x.im;
+            y.re = re - x.re;
+            return y;
+        }
+
+        public Complex Divide(Complex x)
+        {
+            Complex y;
+            if (x.im == 0 || x.re == 0)
+            {
+                throw new Exception("На ноль делить нельзя!");
+            }
+            y.im = im / x.im;
+            y.re = re / x.re;
+            return y;
+        }
+
+        public Complex Multi(Complex x)
+        {
+            Complex y;
+            y.im = re * x.im + im * x.re;
+            y.re = re * x.re - im * x.im;
+            return y;
+        }
+        public override string ToString()
+        {
+            return re + "+" + im + "i";
+        }
+    }
+}
+
+
+
+
+
+
+
